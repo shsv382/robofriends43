c94645024fc4e1614e69e6c8c26589968e34a589
Sergey Shabanov <shsv382@gmail.com>
2024-11-11 19:41:36 +0300

    Добавлен fetch-запрос данных с сервера

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
-const users = [
-    {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-    },
-    {
-        "id": 2,
-        "name": "Ervin Howell",
-        "username": "Antonette",
-        "email": "Shanna@melissa.tv",
-    },
-    {
-        "id": 3,
-        "name": "Clementine Bauch",
-        "username": "Samantha",
-        "email": "Nathan@yesenia.net",
-    },
-    {
-        "id": 4,
-        "name": "Patricia Lebsack",
-        "username": "Karianne",
-        "email": "Julianne.OConner@kory.org",
-    },
-    {
-        "id": 5,
-        "name": "Chelsey Dietrich",
-        "username": "Kamren",
-        "email": "Lucio_Hettinger@annie.ca",
-    },
-    {
-        "id": 6,
-        "name": "Mrs. Dennis Schulist",
-        "username": "Leopoldo_Corkery",
-        "email": "Karley_Dach@jasper.info",
-    },
-    {
-        "id": 7,
-        "name": "Kurtis Weissnat",
-        "username": "Elwyn.Skiles",
-        "email": "Telly.Hoeger@billy.biz",
-    },
-    {
-        "id": 8,
-        "name": "Nicholas Runolfsdottir V",
-        "username": "Maxime_Nienow",
-        "email": "Sherwood@rosamond.me",
-    },
-    {
-        "id": 9,
-        "name": "Glenna Reichert",
-        "username": "Delphine",
-        "email": "Chaim_McDermott@dana.io",
-    },
-    {
-        "id": 10,
-        "name": "Clementina DuBuque",
-        "username": "Moriah.Stanton",
-        "email": "Rey.Padberg@karina.biz",
-    }
-]
+// const users = [
+//     {
+//         "id": 1,
+//         "name": "Leanne Graham",
+//         "username": "Bret",
+//         "email": "Sincere@april.biz",
+//     },
+//     {
+//         "id": 2,
+//         "name": "Ervin Howell",
+//         "username": "Antonette",
+//         "email": "Shanna@melissa.tv",
+//     },
+//     {
+//         "id": 3,
+//         "name": "Clementine Bauch",
+//         "username": "Samantha",
+//         "email": "Nathan@yesenia.net",
+//     },
+//     {
+//         "id": 4,
+//         "name": "Patricia Lebsack",
+//         "username": "Karianne",
+//         "email": "Julianne.OConner@kory.org",
+//     },
+//     {
+//         "id": 5,
+//         "name": "Chelsey Dietrich",
+//         "username": "Kamren",
+//         "email": "Lucio_Hettinger@annie.ca",
+//     },
+//     {
+//         "id": 6,
+//         "name": "Mrs. Dennis Schulist",
+//         "username": "Leopoldo_Corkery",
+//         "email": "Karley_Dach@jasper.info",
+//     },
+//     {
+//         "id": 7,
+//         "name": "Kurtis Weissnat",
+//         "username": "Elwyn.Skiles",
+//         "email": "Telly.Hoeger@billy.biz",
+//     },
+//     {
+//         "id": 8,
+//         "name": "Nicholas Runolfsdottir V",
+//         "username": "Maxime_Nienow",
+//         "email": "Sherwood@rosamond.me",
+//     },
+//     {
+//         "id": 9,
+//         "name": "Glenna Reichert",
+//         "username": "Delphine",
+//         "email": "Chaim_McDermott@dana.io",
+//     },
+//     {
+//         "id": 10,
+//         "name": "Clementina DuBuque",
+//         "username": "Moriah.Stanton",
+//         "email": "Rey.Padberg@karina.biz",
+//     }
+// ]
+
+let users = []
 
 class Robot {
     constructor({ id, name, username, email }) {
@@ -102,7 +104,18 @@ function render(users) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    render(users)
+    // render(users)
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            users = data
+            return users
+        })
+        .then(render)
+        .catch(error => {
+            console.log(error)
+            alert("Что-то пошло не так...")
+        })
 })
 
 const search = document.querySelector(".search")
